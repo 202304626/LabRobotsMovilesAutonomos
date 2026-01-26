b094201d98a47b9810c201ec2ab2293b4fa8e4ca
liiiaamm <liam.esgon@gmail.com>
2026-01-26 19:01:55 +0100

    movement logic for turtlebot

---
+++ sim_ws/src/teleop_nodes/teleop_nodes/websocket_key_node.py
@@ -0,0 +1,44 @@
+import rclpy
+from rclpy.node import Node
+from more_interfaces.msg import KeyTouched
+from sshkeyboard import listen_keyboard
+
+
+class KeyPublisher(Node):
+
+    def __init__(self):
+        super().__init__("websocket_key")
+
+        self.publisher = self.create_publisher(
+            KeyTouched,
+            "listen_key",
+            10,
+        )
+
+        self._running = True
+
+    def on_key_press(self, key):
+        msg = KeyTouched()
+        msg.key = key
+        self.publisher.publish(msg)
+        self.get_logger().info(f"Publishing key: {msg.key}")
+
+
+def main(args=None):
+
+    rclpy.init(args=args)
+
+    node = KeyPublisher()
+
+    listen_keyboard(on_press=node.on_key_press)
+
+    # Loop manual ROS
+    while rclpy.ok():
+        rclpy.spin_once(node, timeout_sec=0.1)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
