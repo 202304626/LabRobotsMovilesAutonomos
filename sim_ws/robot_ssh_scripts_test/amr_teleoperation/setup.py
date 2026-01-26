de6953197d0a91e4acadc371f2a929f4278314f9
202304626 <202304626@alu.comillas.edu>
2026-01-26 19:17:59 +0100

    tbt3 update

---
+++ sim_ws/robot_ssh_scripts_test/amr_teleoperation/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'amr_teleoperation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ros',
+    maintainer_email='202304626@alu.comillas.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'teleoperation_node = amr_teleoperation.teleoperation_node:main'
+        ],
+    },
+)
--- sim_ws/src/tutorial_pubsub_py/tutorial_pubsub_py/publisher_member_function.py
+++ sim_ws/src/tutorial_pubsub_py/tutorial_pubsub_py/publisher_member_function.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0      
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,38 +15,59 @@
 import rclpy  # Import the ROS 2 Python client library (rclpy)
 from rclpy.node import Node  # Import the base Node class to create ROS 2 nodes
 
-from std_msgs.msg import String  # Import the standard String message type (std_msgs/String) to use it to pass data on the topic
+from std_msgs.msg import (
+    String,
+)  # Import the standard String message type (std_msgs/String) to use it to pass data on the topic
 
 
-class MinimalPublisher(Node):  # Define a node class that will publish messages, it inherits from the class Node (Node is the parent class)
-
+class MinimalPublisher(
+    Node
+):  # Define a node class that will publish messages, it inherits from the class Node (Node is the parent class)
     def __init__(self):  # Constructor: runs when the node object is created
-        super().__init__('minimal_publisher')  # Initialize the Node with the name "minimal_publisher", usign the parent's constructor
+        super().__init__(
+            "minimal_publisher"
+        )  # Initialize the Node with the name "minimal_publisher", usign the parent's constructor
 
-        self.publisher_ = self.create_publisher(String, 'topic', 10)  # Create a publisher on the topic called 'topic' with queue size of 10 (queue is a quality of service)
+        self.publisher_ = self.create_publisher(
+            String, "topic", 10
+        )  # Create a publisher on the topic called 'topic' with queue size of 10 (queue is a quality of service)
         # It has to be a method defined in the parent class: Node
 
         timer_period = 0.5  # seconds  # Set the timer period (publish every 0.5 s)
-        self.timer = self.create_timer(timer_period, self.timer_callback)  # Create a timer that calls the function timer_callback periodically (every timer_period secs)
+        self.timer = self.create_timer(
+            timer_period, self.timer_callback
+        )  # Create a timer that calls the function timer_callback periodically (every timer_period secs)
         # It has to be a method defined in the parent class: Node
 
         self.i = 0  # Initialize a counter used to change the message content each time
 
-    def timer_callback(self):  # Callback function executed every time the timer triggers, because we created the self.timer on the __init__
+    def timer_callback(
+        self,
+    ):  # Callback function executed every time the timer triggers, because we created the self.timer on the __init__
         msg = String()  # Create a new String message object
-        msg.data = 'Hello World: %d' % self.i  # Fill the string message payload with text + the current counter value (a decimal value, the self.i value)
-        self.publisher_.publish(msg)  # Publish the message to the configured topic (already specified when created the publisher itself)
+        msg.data = (
+            "Hello World: %d" % self.i
+        )  # Fill the string message payload with text + the current counter value (a decimal value, the self.i value)
+        self.publisher_.publish(
+            msg
+        )  # Publish the message to the configured topic (already specified when created the publisher itself)
 
-        self.get_logger().info('Publishing: "%s"' % msg.data)  # Print an info log to the terminal with the sent data
+        self.get_logger().info(
+            'Publishing: "%s"' % msg.data
+        )  # Print an info log to the terminal with the sent data
         self.i += 1  # Increment the counter for the next timer call
 
 
 def main(args=None):  # Main entry point of the script (optionally receives CLI arguments)
     rclpy.init(args=args)  # Initialize the ROS 2 communication layer for this Python process
 
-    minimal_publisher = MinimalPublisher()  # Create an instance of the publisher node, using the class MinimalPublisher()
+    minimal_publisher = (
+        MinimalPublisher()
+    )  # Create an instance of the publisher node, using the class MinimalPublisher()
 
-    rclpy.spin(minimal_publisher)  # Keep the node alive and process callbacks (e.g., the timer callback)
+    rclpy.spin(
+        minimal_publisher
+    )  # Keep the node alive and process callbacks (e.g., the timer callback)
     # This makes the node keep listening to callbacks
 
     # Destroy the node explicitly  # Cleanup: destroy the node object explicitly
@@ -58,5 +79,5 @@ def main(args=None):  # Main entry point of the script (optionally receives CLI 
     rclpy.shutdown()  # Shutdown rclpy and cleanly stop ROS 2 communications
 
 
-if __name__ == '__main__': 
-    main()  
+if __name__ == "__main__":
+    main()
